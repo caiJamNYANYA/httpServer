f3da7bec2bb942bb72af72ce0931917d3066461e
caiJamNYANYA <lzr20081130@gmail.com>
2024-01-01 09:57:46 +0800

    main.go

--- main.go
+++ main.go
@@ -9,15 +9,17 @@ import (
 	"embed"	
 	"bufio"
 	// "net"
+	"sync"
 	"strings"
 	"archive/tar"
 	"compress/gzip"
 	"math/rand"
 	"path/filepath"
-	"syscall"
-	"os/signal"
+/*	"syscall"
+	"os/signal"*/
 	"net/http"
 	"github.com/ulikunitz/xz"
+	"github.com/chzyer/readline"
 )
 
 //go:embed icon/*
@@ -123,11 +125,10 @@ func main() {
 		case "-p", "--port" :
 			if i + 2 > len(os.Args) {
 				cPrint("Usage:")
-				cPrint("-p int\vlisten port (default 5050)")
+				cPrint("-p, --port\v似乎需要输入一个数字的样子(笑)")
 				os.Exit(0)
 			} else {
 				portStr := os.Args[i+1]
-				//	var port int
 				_, err := fmt.Sscanf(portStr, "%d", &port)//转成数字是为了比较的……
 				if err != nil {
 					cPrint("喂！你有在输入端口吗(\"▔□▔)/\n错误的端口(´･_･`)")
@@ -139,16 +140,17 @@ func main() {
 			}
 		case "-h", "--help" :
 			cPrint("Usage: shrd [PATH]... [-p <PORT>]")
-			cPrint("\t-p, --port 似乎需要一个整数的样子(笑)")
+			cPrint("\t-p, --port 似乎需要一个数字的样子(笑)")
 			cPrint("\t-h, --help 可莉不知道哦(●´ϖ`●)")
-			textColoful("少女乞讨中");fmt.Println("...")
+			textColoful("少女讨饭中");fmt.Println("...")
 			os.Exit(0)
 
 		}
 	}
 
 
-	textColoful("Http Server\n")
+	
+	textColoful("Ciallo～(∠・ω< )⌒☆\n\n")
 	randomNumbr = ("____" + randomString(12))//共享文件夹名称
 	homePath = fmt.Sprintf("%s/%s",os.Getenv("HOME"),randomNumbr)//共享文件夹绝对路径
 	os.Mkdir(homePath, os.ModePerm)//创建共享文件夹
@@ -158,19 +160,13 @@ func main() {
 	// 打印接收到的数据
 
 	pathArgs := make([]string, 2)//定义存储路径的数组；长度为2用于没有输入参数时定义默认值
+	os.Args[0] = ""
 
 
 	if (len(os.Args) == 1/*数组第0个元素默认是启动程序*/) || (port != 5050 && len(os.Args) == 3/*加了port参数-p之后长度变成3*/) {
-		os.Args[0] = ""
 		pathArgs[1] = "."//定义默认路径为当前目录
-	} else if os.Args[1] == "-p"/*使用了端口值并且使用了路径时*/ {
-		os.Args[0] = ""
-		pathArgs = os.Args
-		pathArgs[1] = randomString(12)//将-p参数替换为随机字符一次，以防将当前目录下与-p同名的文件夹给分享出去……
-		pathArgs[2] = randomString(12)//将端口这一参数替换为随机的字符串，以防将目录下与端口名称一样的目录给分享出去……
 	} else {
-		os.Args[0] = ""
-		pathArgs = os.Args//只使用了路径参数时
+		pathArgs = os.Args
 	}
 
 	for _, value := range pathArgs {//读取数组获取绝对路径
@@ -178,12 +174,13 @@ func main() {
 		_, err := os.Stat(absDir)//检查绝对路径是否有效，只输出有效路径
 		if err != nil {
 			//该路径不存在
+			fmt.Print("\x1b[38;5;211m):\t" + absDir + "\n")
 		} else {
 			pathOrign = append(pathOrign,absDir)//将有效的路径存储进pathOrign数组
 		}
 	}
 
-	if (len(pathOrign)) == 1 {//如果没有一个正确路径时退出程序，如果想使用空目录并手动添加文件可以删掉这段……
+	if (len(pathOrign)) == 1 {//如果没有一个正确路径时退出程序，
 		fmt.Println("\n\x1b[38;5;211m这就是你输入的路径?(_^_)_")
 		rm(homePath)//清除分享目录
 		os.Exit(1)
@@ -191,7 +188,7 @@ func main() {
 
 	for i := 1 ; i <= len(pathOrign) - 1/*因为数组第0个元素是程序名称，所有原来数组长度为输入的路径+1,如果路径个数为1时，只需要循环一此，以此类推*/; i++ {
 		fromPath := pathOrign[i]//读取需要链接的原始路径
-		dir, file := filepath.Split(fromPath)//判断路径类型；避免bug出现
+		dir, file := filepath.Split(fromPath)//判断根目录避免bug出现
 		var pathName string
 		if dir == "/" && file == "" {//unix类系统的/作为路径分隔符是唯一不能用作文件名的字符，所以直接以/为名称给根目录创建链接会出错
 			pathName = "R0OT"//更改根目录的目标链接名
@@ -225,52 +222,86 @@ func main() {
 			fmt.Println(err)
 			rm(homePath)
 			os.Exit(1)
-		//	panic(err)
 		}
 	}
 
-	sigChan := make(chan os.Signal, 1)//用于退出时清理共享文件夹;监听ctrl+c
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT)
-	go func() {//退出时执行的
-		<-sigChan
-		if rm(homePath) != nil {//移除共享文件夹
-
-		fmt.Printf("\n\x1b[38;5;211m似乎并没有删除什么 (@_@)")
-		} else {
-			fmt.Println("\n\x1b[38;5;85m似乎删掉了什么（￣▽￣）",randomNumbr)
-			}
-		os.Exit(0)
-	}()
-	http.HandleFunc("/", handler)//挂载根目录
-	http.HandleFunc("/favicon.ico", serveFavicon)
-	address := fmt.Sprintf(":%d", port)//http监听地址:端口
-	fmt.Printf("\n\x1b[38;5;85mHTTP%s @ %s (・o・)\n\n\033[0m", address, homePath)
-	fmt.Printf("\x1b[38;5;%dm",clr[rand.Intn(len(clr))])
-	err := http.ListenAndServe(address, nil)//开启http服务
-	if err != nil {
-		if rm(homePath) != nil {//移除共享文件夹
-		} else {
-			if port < 1024 {
-				errstr := fmt.Sprintf("%v",err)
-				if (errstr[len(errstr)-2:]) == "ed" {
-					fmt.Println("\x1b[38;5;211m你什么身份呀(╯°口°)╯(┴—┴这个端口也是你能用的！")
-					fmt.Println("似乎删掉了什么 (゜o゜)",randomNumbr)
-					os.Exit(1)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		http.HandleFunc("/", handler)//挂载根目录
+		http.HandleFunc("/favicon.ico", serveFavicon)
+		address := fmt.Sprintf(":%d", port)//http监听地址:端口
+		fmt.Printf("\n\x1b[38;5;85mHTTP%s @ %s (・o・)\n\n\033[0m", address, homePath)
+		err := http.ListenAndServe(address, nil)//开启http服务
+		if err != nil {
+			if rm(homePath) != nil {//移除共享文件夹
+			} else {
+				if port < 1024 {
+					errstr := fmt.Sprintf("%v",err)
+					if (errstr[len(errstr)-2:]) == "ed" {
+						fmt.Println("\x1b[38;5;211m你什么身份呀(╯°口°)╯(┴—┴这个>端口也是你能用的！")
+						fmt.Println("已清理产物 (゜o゜)",randomNumbr)
+						os.Exit(1)
+					} else {
+						fmt.Println("\x1b[38;5;211m该端口无了 ≡≡ﾍ( ´Д`)ﾉ")
+						fmt.Println("已清理产物 (゜o゜)",randomNumbr)
+						os.Exit(1)
+					}
 				} else {
 					fmt.Println("\x1b[38;5;211m该端口无了 ≡≡ﾍ( ´Д`)ﾉ")
-					fmt.Println("似乎删掉了什么 (゜o゜)",randomNumbr)
+					fmt.Println("已清理产物 (゜o゜)",randomNumbr)
 					os.Exit(1)
 				}
-			} else {
-
-				fmt.Println("\x1b[38;5;211m该端口无了 ≡≡ﾍ( ´Д`)ﾉ")
-				fmt.Println("似乎删掉了什么 (゜o゜)",randomNumbr)
-				os.Exit(1)
 			}
 		}
-	}
-	select {}
+	}()
 
+	go func() {
+		l, err := readline.NewEx(&readline.Config{
+			Prompt:"> ",
+			HistoryFile:"/tmp/readline.tmp",
+			InterruptPrompt:"^C",
+			EOFPrompt:"exit",
+		})
+		if err != nil {
+			fmt.Println("Error: ", err)
+			return
+		}
+
+		defer l.Close()
+		for {
+			fmt.Printf("\x1b[38;5;%dm%s",clr[rand.Intn(len(clr))])
+			line, err := l.Readline()
+		//	fmt.Print(err)
+			if err == readline.ErrInterrupt || fmt.Sprint(err) == "EOF" {
+				rm(homePath)
+				fmt.Println("\n\x1b[38;5;85m已清理产物（￣▽￣）",randomNumbr)
+				os.Exit(0)
+			}
+			if line == "" {
+				continue
+			}
+			for _, path := range pathNameArgs {
+				if strings.Contains(path, line) {
+					downloadAddr := fmt.Sprintf("localhost:%d/%s"/*,ipArgs[0]*/,port,path)
+					fmt.Printf("\x1b[38;5;%dm%s\n",clr[rand.Intn(len(clr))],downloadAddr)
+//					break
+
+				}
+			}
+			if line == "/ls" {
+				fmt.Println(pathNameArgs)
+			}
+//			if err != nil {
+//				break
+//			}
+
+			l.SaveHistory(line)
+		}
+	}()
+
+
+	wg.Wait()
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -334,6 +365,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 				tarType = "t"
 				fileName = pathName + ".tar"
 		}
+
 		tarTo = tarPath + "/" + fileName
 		err := tarGzFiles(tarType, tarTo, tarFrom)
 		if err != nil {
@@ -352,13 +384,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fileInfo, err := os.Lstat(path)
 			if err != nil {
 				fmt.Printf("\x1b[38;5;%dm请求路径 ):\t%s\n", clr[rand.Intn(len(clr))],r.URL.Path)//打印请求
-				fmt.Fprintf(w,"No Found Funky(_^_)_")
+				if r.Header.Get("User-Agent")[:1] == "c" {//判断是否使用curl
+					fmt.Fprintf(w,"\x1b[38;5;%dm",clr[rand.Intn(len(clr))])
+				}
+				fmt.Fprintf(w,"这里什么也没有呢，我都要吃草了(_^_)_，既然来了就住下吧……我可没有任何多余的东西呢…\n")
 				fmt.Println(err)
 				return
 			} else {
 				fmt.Printf("\x1b[38;5;%dm请求路径 (:\t%s\n", clr[rand.Intn(len(clr))],r.URL.Path)//打印请求
 			}
-		if fileInfo.IsDir() {
+
+		if fileInfo.IsDir() {//当访问的网络路径是目录类型
 htmlcode := `<meta charset="UTF-8">
 <script>
 coloR = ["#82b1ff","#ff89cf","#FB7299","#6dc781"];
@@ -404,30 +440,30 @@ h1 {
 			Link, _ := os.ReadDir(path)
 			for _, Link := range Link {
 				switch fmt.Sprintln(Link)[:1] {//判断路径类型
+				case "d" :
+					dirLink = append(dirLink,r.URL.Path + Link.Name())
+					dirName = append(dirName,Link.Name())
+				case "-" :
+					fileLink = append(fileLink,r.URL.Path + Link.Name())
+					fileName = append(fileName,Link.Name())
+				case "L" :
+					var realLink string
+					absPath, _ := filepath.EvalSymlinks(path + Link.Name())
+					realLink, _ = filepath.Abs(absPath)
+					fileInfo, _ := os.Lstat(realLink)
+					switch fmt.Sprintln(fileInfo.Mode())[:1] {
 					case "d" :
 						dirLink = append(dirLink,r.URL.Path + Link.Name())
 						dirName = append(dirName,Link.Name())
 					case "-" :
 						fileLink = append(fileLink,r.URL.Path + Link.Name())
 						fileName = append(fileName,Link.Name())
-					case "L" :
-						var realLink string
-						absPath, _ := filepath.EvalSymlinks(path + Link.Name())
-						realLink, _ = filepath.Abs(absPath)
-						fileInfo, _ := os.Lstat(realLink)
-						switch fmt.Sprintln(fileInfo.Mode())[:1] {
-							case "d" :
-								dirLink = append(dirLink,r.URL.Path + Link.Name())
-								dirName = append(dirName,Link.Name())
-							case "-" :
-								fileLink = append(fileLink,r.URL.Path + Link.Name())
-								fileName = append(fileName,Link.Name())
-//							case "L" :
-//								这里大概不会有软链接了的……
+//					case "L" :
+//						这里大概不会有软链接了的……
 						}
 				}
 			}
-			sort.Slice(fileName, func(i, j int) bool {
+			sort.Slice(fileName, func(i, j int) bool {//按后缀排序
 				suffixI := getFileSuffix(fileName[i])
 				suffixJ := getFileSuffix(fileName[j])
 				_Rs := false
@@ -438,6 +474,7 @@ h1 {
 				}
 					return _Rs
 			})
+
 			if r.Header.Get("User-Agent")[:1] == "c" {//判断是否是curl请求
 				for i, _ := range dirLink {//打印目录列表
 					w.Write([]byte(fmt.Sprintf("\x1b[38;5;%dm",clr[rand.Intn(len(clr))]) + dirName[i] + "/\n"))
@@ -459,7 +496,7 @@ h1 {
 				w.Write([]byte("</pre>"))
 			}
 
-		} else {
+		} else {//当访问的网络路径是文件类型
 			http.ServeFile(w, r, path)
 		}
 //*/
